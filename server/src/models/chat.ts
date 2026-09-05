e21fc10defb05ed81a61799716aad0fe5526dcc5
Nikolai Gavriushin <gna@pm.me>
2026-09-05 17:02:39 +0400

    Create models

---
+++ server/src/models/chat.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+export default Chat;
---
+++ server/src/models/message.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chat',
+    required: true,
+  },
+  role: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
