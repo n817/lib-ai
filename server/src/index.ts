da0ba54be2d54a1bab8b7cc0ab2e408e06491106
Nikolai Gavriushin <gna@pm.me>
2026-09-05 16:43:23 +0400

    Add MongoDB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose';
 
 import { logger } from './middleware/logger.js';
 import { errorHandler, notFoundHandler } from './middleware/error.js';
 import mainRouter from "./routes/index.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger);
 
@@ -17,6 +21,14 @@ app.get("/health", (req, res): void => {
     "error": null
   });
 });
+
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Connection error', err);
+  });
 
 app.use(express.json());
 
