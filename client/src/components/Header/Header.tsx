94dc083cb7a70eed1038aa81be8b763fdd8a0dbf
Nikolai Gavriushin <gna@pm.me>
2026-09-09 14:08:33 +0400

    Add Header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,26 @@
-import './Header.css';
+import { NavLink } from "react-router";
+import Logo from "../../assets/logo.svg";
+
+import "./Header.css";
 
 export default function Header() {
-    return(
-        <header className="header">Header</header>
-    )
+  function getNavLinkClass({ isActive }: { isActive: boolean }) {
+    return isActive
+      ? "header__nav-link header__nav-link_active"
+      : "header__nav-link";
+  }
+
+  return (
+    <header className="header">
+      <img src={Logo} alt="bioAI logo" className="header__logo" />
+      <nav className="header__nav">
+        <NavLink to="/knowledge-base" className={getNavLinkClass}>
+          Knowledge Base
+        </NavLink>
+        <NavLink to="/chat" className={getNavLinkClass}>
+          Chat
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
